31de0607bcfdc9b6425a526bc1a7824e157bca4f
tafseerahmed <samsamsamrox1212@gmail.com>
2017-11-18 18:21:56 +0530

    Update encr.py
    Completed more steps in the algorithm

--- encr.py
+++ encr.py
@@ -14,6 +14,17 @@ GLOBAL Constants
 x0=10
 y0=8/3
 z0=28
+#DNA-Encoding RULE #1 A = 00, T=01, G=10, C=11
+dna={}
+dna["00"]="A"
+dna["01"]="T"
+dna["10"]="G"
+dna["11"]="C"
+#DNA xor
+dna["AA"]=dna["TT"]=dna["GG"]=dna["CC"]="A"
+dna["AG"]=dna["GA"]=dna["TC"]=dna["CT"]="G"
+dna["AC"]=dna["CA"]=dna["GT"]=dna["TG"]="C"
+dna["AT"]=dna["TA"]=dna["CG"]=dna["GC"]="T"
 
 def image_selector():                           #returns path to selected image
     path = "NULL"
@@ -80,14 +91,65 @@ def decompose_matrix(iname):
             G = np.asmatrix(img)
         else:
             R = np.asmatrix(img)
-    print (np.unpackbits(B,axis=1))
+    B = np.unpackbits(B,axis=1)
+    G = np.unpackbits(G,axis=1)
+    R = np.unpackbits(R,axis=1)
+    print(blue.shape)
+    return B,G,R
+
+def dna_encode(b,g,r,key):
+    m,n = b.shape
+    r_enc= np.chararray((m,int(n/2))).astype(str)
+    g_enc= np.chararray((m,int(n/2))).astype(str)
+    b_enc= np.chararray((m,int(n/2))).astype(str)
+    for color,enc in zip((b,g,r),(b_enc,g_enc,r_enc)):
+        idx=0
+        for j in range(0,m):
+            for i in range(0,n,2):
+                if idx==(n/2):
+                    idx=0
+                enc[j,idx]=dna["{0}{1}".format(color[j,i],color[j,i+1])]
+                idx+=1
+    #encoded key matrix
+    key_bin = bin(int(key, 16))[2:].zfill(256)
+    Mk = np.zeros((m,n),dtype=np.uint8)
+    x=0
+    for j in range(0,m):
+            for i in range(0,n):
+                Mk[j,i]=key_bin[x%256]
+                x+=1
+    
+    Mk_enc=np.chararray((m,int(n/2))).astype(str)
+    idx=0
+    for j in range(0,m):
+        for i in range(0,n,2):
+            if idx==(n/2):
+                idx=0
+            Mk_enc[j,idx]=dna["{0}{1}".format(Mk[j,i],Mk[j,i+1])]
+            idx+=1
+    return b_enc,g_enc,r_enc,Mk_enc
+
+def xor_operation(b,g,r,mk):
+    m,n = b.shape
+    bx=np.chararray((m,n)).astype(str)
+    gx=np.chararray((m,n)).astype(str)
+    rx=np.chararray((m,n)).astype(str)
+    
+    for i in range(0,m):
+        for j in range (0,n):
+            bx[i,j] = dna["{0}{1}".format(b[i,j],mk[i,j])]
+            gx[i,j] = dna["{0}{1}".format(g[i,j],mk[i,j])]
+            rx[i,j] = dna["{0}{1}".format(r[i,j],mk[i,j])]
+    return bx,gx,rx 
 
 #program exec9
 if __name__ == "__main__":
     file_path = image_selector()
     key = securekey(file_path)
     update_lorentz(key)
-    decompose_matrix(file_path)
+    blue,green,red=decompose_matrix(file_path)
+    blue_e,green_e,red_e,Mk_e=dna_encode(blue,green,red,key)
+    blue_final, green_final, red_final = xor_operation(blue_e,green_e,red_e,Mk_e)
 
 
     
